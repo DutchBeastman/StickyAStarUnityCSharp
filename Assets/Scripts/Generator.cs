53de00bf79cf6b11471c7d88acb5d18fb77fb3f2
DutchBeastman <Fabianverk@hotmail.com>
2015-06-17 00:02:13 +0200

    Commiting Further Progress
    Progress made in generation of HexaGrid

--- Assets/Scripts/Generator.cs
+++ Assets/Scripts/Generator.cs
@@ -1,24 +1,42 @@
 ﻿using UnityEngine;
 using System.Collections;
-using System;
+
+
 
 public class Generator : MonoBehaviour {
-    public enum Tiles { Grass, Mountain, Forest, Desert, Water};
+    //public enum Tiles { Grass, Mountain, Forest, Desert, Water};
 
-    
+    [SerializeField]private GameObject [] tiles; 
     [SerializeField]private int tilesX;
     [SerializeField]private int tilesY;
 	// Use this for initialization
 	protected void Start () {
-        Tiles tiles = (Tiles)UnityEngine.Random.Range(0, Enum.GetValues(typeof(Tiles)).Length);
+        //Tiles tiles = (Tiles)UnityEngine.Random.Range(0, Enum.GetValues(typeof(Tiles)).Length);
+        Generation();
 	}
     private void Generation()
     {
-        for (int i = 0; i < tilesX; i++)
+        for (int x = 0; x < tilesX; x++)
         {
-            Tiles tiles = (Tiles)UnityEngine.Random.Range(0, Enum.GetValues(typeof(Tiles)).Length);
-            
 
+            Vector3 position = Vector3.right * x;
+
+            for (int z = 0; z < tilesY; z++)
+            {
+                position += Vector3.forward / 1.4f;
+
+                if ((z % 2) == 0)
+                {
+                    position += Vector3.right / 2;
+                }
+                else
+                {
+                    position -= Vector3.right / 2;
+                }
+
+                //position -= Vector3.forward * 3/4;
+                Instantiate(tiles[Random.Range(0, tiles.Length)], position, Quaternion.identity);                
+            }
         }
 
     }
