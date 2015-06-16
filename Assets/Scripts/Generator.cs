5c18a3a1f7459b9b8de78e52dabf28bd2b6e80bf
DutchBeastman <Fabianverk@hotmail.com>
2015-06-16 22:10:39 +0200

    Commiting Progress

---
+++ Assets/Scripts/Generator.cs
@@ -0,0 +1,30 @@
+﻿using UnityEngine;
+using System.Collections;
+using System;
+
+public class Generator : MonoBehaviour {
+    public enum Tiles { Grass, Mountain, Forest, Desert, Water};
+
+    
+    [SerializeField]private int tilesX;
+    [SerializeField]private int tilesY;
+	// Use this for initialization
+	protected void Start () {
+        Tiles tiles = (Tiles)UnityEngine.Random.Range(0, Enum.GetValues(typeof(Tiles)).Length);
+	}
+    private void Generation()
+    {
+        for (int i = 0; i < tilesX; i++)
+        {
+            Tiles tiles = (Tiles)UnityEngine.Random.Range(0, Enum.GetValues(typeof(Tiles)).Length);
+            
+
+        }
+
+    }
+	
+	// Update is called once per frame
+	protected void Update () {
+	
+	}
+}
