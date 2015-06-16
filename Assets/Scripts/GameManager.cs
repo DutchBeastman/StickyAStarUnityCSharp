863f3063dee73ba38575ef3a278b46808f92ba91
DutchBeastman <Fabianverk@hotmail.com>
2015-06-16 17:21:09 +0200

    First Commit

---
+++ Assets/Scripts/GameManager.cs
@@ -0,0 +1,38 @@
+﻿using UnityEngine;
+using System.Collections;
+using System;
+using System.Collections.Generic;
+
+namespace Pathing
+{
+    public class GameManager : MonoBehaviour
+    {
+        private IAStarNode start, goal;
+
+
+        void Update()
+        {
+            if (Input.GetMouseButtonDown(0))
+            {
+
+                Ray ray;
+                RaycastHit hit;
+                ray = Camera.main.ScreenPointToRay(Input.mousePosition);
+                if (Physics.Raycast(ray,out hit))
+                {
+                    if (hit.transform.tag == "Node" && start == null)
+                    {
+                        start = (IAStarNode) GetComponent(typeof(IAStarNode));
+                        Debug.Log("Start == " + start);
+                    }
+                    if (hit.transform.tag == "Node" && goal == null && start != null)
+                    {
+                        goal = (IAStarNode)GetComponent(typeof(IAStarNode));
+                        Debug.Log("Goal == " + goal);
+                    }
+                }
+            }
+
+        }
+    }
+}
---
+++ Assets/Scripts/Node.cs
@@ -0,0 +1,43 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+using System;
+
+namespace Pathing
+{
+    public class Node : IAStarNode
+    {
+
+        /*private static List<IAStarNode> open = new List<IAStarNode>();
+
+        [SerializeField]
+        private int cost;
+        public Node(int _cost, IAStarNode _aNode)
+        {
+                        
+            cost = _cost;
+        }*/
+        
+
+        public IEnumerable<IAStarNode> Neighbours
+        {
+            get
+            {
+                throw new NotImplementedException();
+            }
+        }
+
+        public float CostTo(IAStarNode neighbour)
+        {
+
+            throw new NotImplementedException();
+        }
+
+        public float EstimatedCostTo(IAStarNode goal)
+        {
+            throw new NotImplementedException();
+        }
+
+
+    }
+}
